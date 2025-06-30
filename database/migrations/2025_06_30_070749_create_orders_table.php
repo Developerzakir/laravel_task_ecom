fd84d51311f88e9fd2483f63400d2ff53c5071b6
zakir Hossen <zakirhossen082@gmail.com>
2025-06-30 13:20:08 +0600

    created all neccessary migrations files

---
+++ database/migrations/2025_06_30_070749_create_orders_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+           $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('order_number')->unique();
+            $table->enum('status', ['pending', 'processing', 'completed', 'declined'])->default('pending');
+            $table->decimal('total_amount', 10, 2);
+            $table->integer('item_count');
+            $table->boolean('is_paid')->default(false);
+            $table->enum('payment_method', ['cash_on_delivery', 'paypal', 'stripe', 'card'])->nullable();
+            $table->string('shipping_full_name');
+            $table->string('shipping_address');
+            $table->string('shipping_city');
+            $table->string('shipping_state')->nullable();
+            $table->string('shipping_zip_code');
+            $table->string('shipping_phone');
+            $table->text('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
